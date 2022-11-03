efa6979c6edf92a0b0c42ddc319d8b64c2d4f315
Sayfuddin Naeem <44950101+Sayfuddin-Naeem@users.noreply.github.com>
2022-11-03 20:37:00 +0600

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,8 +1,8 @@
 <?php
 define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "root@");
-define("DB_NAME", "db_blog");
+define("DB_USER", "id19797496_root");
+define("DB_PASS", "y]j}X0ak(ng4Vrd+");
+define("DB_NAME", "id19797496_db_blog");
 define("TITLE", "historicalsitesintheworld");
 define("KEYWORDS", "Historical Sites of Pakistan, Historical Sites of Bangladesh, Historical Sites of Soudi Arab, Historical Sites of Turkey, Historical Sites of India");
 
